ea26a600d0c2faac00e53015b98c664d23386798
Ryan Park <mrswimman563@gmail.com>
2016-07-27 23:42:26 -0600

    Music, Level 10, Brand new door switch feature
    Contains the music, a rough level 10, and a new feature that unlocks
    doors.  Yeah!  Challenges!

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public Door _door;
+	private SpringController spring;
+	private BallController ball;
+	// Use this for initialization
+	void Start () {
+		spring = GameObject.Find("spring").GetComponent<SpringController>();
+		ball = GameObject.Find("ball").GetComponent<BallController>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D box)
+	{
+		if (box.name == "spring" || box.name == "ball") {
+			Debug.Log ("Open Sesame");
+			_door.OpenDoor ();
+		} 
+
+	}
+}
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public GameObject _door;
+	private SpringController spring;
+	private BallController ball;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void OpenDoor ()
+	{
+		_door.SetActive (false);
+	}
+}
